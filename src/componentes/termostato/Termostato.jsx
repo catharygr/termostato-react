51c69883211fd4892e8cdb2a0628b549075b4369
catharygr <103951447+catharygr@users.noreply.github.com>
2023-08-26 12:38:11 +0100

    Trabajando con CSS

--- src/componentes/termostato/Termostato.jsx
+++ src/componentes/termostato/Termostato.jsx
@@ -5,17 +5,20 @@ export default function Termostato() {
   return (
     <div className={styles.termostato}>
       <h1>Termostato</h1>
-      <div>40</div>
-      <div>
-        <input type="text" name="" id="" placeholder="Modo" />
-      </div>
-      <div>
-        <button>
-          <ChevronDown />
-        </button>
-        <button>
-          <ChevronUp />
-        </button>
+      <div className={styles.display}>40</div>
+      <div className={styles.grupo_botones}>
+        <div className={styles.grupo}>
+          <label htmlFor=""></label>
+          <input type="text" name="" id="" placeholder="Modo" />
+        </div>
+        <div className={styles.grupo_flechas}>
+          <button className={styles.btn_down}>
+            <ChevronDown />
+          </button>
+          <button className={styles.btn_up}>
+            <ChevronUp />
+          </button>
+        </div>
       </div>
     </div>
   );
